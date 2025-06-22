6ed4f15275323055c669e99a25973655b1c48983
https://github.com/naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-06-22 21:29:36 +0200

    Arraylistexample is add

---
+++ ArrayListTest.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+ class ArrayListTest { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.add("Toyota");
+    cars.add("Renolt");
+    Collections.sort(cars);
+    for (String i : cars) {
+      System.out.println(i);
+    }
+  } 
+}
