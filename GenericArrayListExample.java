c0dbee5e31b859a20188d8d1528acbe5262ed6d6
https://github.com/naseerAhmadKhanDK <naseeruomian50@gmail.com>
2025-06-23 13:27:12 +0200

    GenericArrayList example is add

---
+++ GenericArrayListExample.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericArrayListExample {
+    
+    public static void main(String[] args){
+       List<Integer> intList=new ArrayList<Integer>();
+       intList.add(3);
+       printList(intList);
+
+      // List<Cat> catList=new ArrayList<>();
+      // catList.add(new Cat());
+     //  printList(catList);
+
+    }
+    private static void printList(List<?> myList){
+        // ? when exactly dont know what to pass
+    System.out.println(myList);
+    
+    }
+    
+}
+
